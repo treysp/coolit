6135c1fa4fb59b05574cb7d6954e93e1255256ab
Trey Spiller <treyspiller@gmail.com>
2019-03-29 17:18:40 -0400

    Initial commit

---
+++ R/score_jp2_image_dir.R
@@ -0,0 +1,93 @@
+#' Score a directory of jp2 images with a Keras model
+#'
+#' @param jp2_dir Path to directory containing .jp2 image files
+#' @param jp2_aux_path Path to directory containing .jp2.aux.xml files with
+#'                     metadata about .jp2 image with corresponding name
+#'
+#' @param model_params_dput_file
+#' @param model_h5_weights Saved h5 weights from a trained keras model, used to score
+#'                         the image tiles.
+#'
+#' @param score_outdir Path to directory where results of image scoring should be save
+#' @param compress_score_rds Should saved score results be compressed
+#' @param return_score Should list of score results be returned as an R object by the function
+#'
+#' @param tile_overlap Number of pixel overlap in adjacent tiles (in both X and Y directions).
+#'                       See \code{\link{calc_tile_corners}}.
+#'
+#' @param complete_image See \code{\link{calc_tile_corners}}.
+#'
+#' @return List of data frame with one row for each tile, containing:
+#' - \code{jp2_path}
+#' - \code{jp2_aux_path}
+#' - Numeric tile ID number
+#' - Tile corner cells in source jp2 pixel values
+#' - List-column containing each tile's extent based on the source jp2's projected raster
+#' - sf geometry column containing each tile's sf polygon
+#' - List-column containing tile's RGB layers in a 4d array of dimension
+#' \[1, \code{tile_n_cols}, \code{tile_n_rows}, 3\]
+#' - Model predicted probability for each image tile
+#'
+#' If score \code{outpath != NULL} each returned data frame will be saved to outpath as an
+#' RDS file, compressed if \code{compress_score_rds == TRUE}.
+#'
+#' @export
+#' @importFrom pbapply pblapply
+#' @importFrom keras load_model_hdf5
+score_jp2_image_dir <- function(jp2_dir, jp2_aux_dir,
+                                model_params_dput_file, model_h5_weights,
+                                score_outdir = NULL, compress_score_rds = FALSE,
+                                return_score = TRUE, verbose = FALSE,
+                                tile_overlap = 0, complete_image = FALSE) {
+  # make paired list of img and aux files
+  jp2_files <- list.files(jp2_dir)
+  jp2_files <- jp2_files[grepl("\\.jp2$", jp2_files)]
+  jp2_file_names <- data.frame(
+    stub = str_match(jp2_files, "(.*?)\\.jp2$")[, 2],
+    img_file = jp2_files,
+    stringsAsFactors = FALSE
+  )
+
+  jp2_aux_files <- list.files(jp2_aux_dir)
+  jp2_aux_files <- jp2_aux_files[grepl("\\.jp2\\.aux\\.xml$", jp2_aux_files)]
+  jp2_aux_files <- data.frame(
+    stub = str_match(jp2_aux_files, "(.*?)\\.jp2\\.aux\\.xml$$")[, 2],
+    aux_file = jp2_aux_files,
+    stringsAsFactors = FALSE
+  )
+
+  jp2_file_names <- merge(jp2_file_names, jp2_aux_files, by = "stub")
+
+  if (nrow(jp2_file_names) != length(jp2_files)) {
+    warning("Only scoring images with a corresponding aux file")
+  }
+
+  jp2_file_names <- jp2_file_names[
+    !is.na(jp2_file_names$img_file) & !is.na(jp2_file_names$aux_file)
+    ,
+    ]
+
+  images_to_score <- split(jp2_file_names, 1:nrow(jp2_file_names))
+
+  # load model and score
+  my_model_params <- eval(parse(model_params_dput_file))
+  scoring_model <- load_model_hdf5(model_h5_weights)
+
+  out <- pblapply(images_to_score, function(x) {
+
+    score_jp2_image(
+      model_params = my_model_params,
+      scoring_model = scoring_model,
+      jp2_path = file.path(jp2_dir, x[["img_file"]]),
+      jp2_aux_path = file.path(jp2_aux_dir, x[["aux_file"]]),
+      score_outpath = file.path(score_outdir, paste0("img_", x[["stub"]], "_scores.rds")),
+      return_score = return_score,
+      verbose = verbose,
+      tile_overlap = tile_overlap,
+      complete_image = complete_image
+    )
+
+  })
+
+  out
+}
---
+++ R/score_test_images.R
@@ -0,0 +1,82 @@
+#' score training test-set images
+#'
+#' @export
+#' @importFrom keras image_to_array image_load array_reshape predict_proba
+#'             keras_model
+#' @importFrom pbapply pblapply
+#' @importFrom abind abind
+score_test_images <- function(img_dir) {
+before <- Sys.time()
+
+valid_files <- list.files(params$valid_dir, full.names = TRUE, recursive = TRUE)
+
+img_dims <- dim(image_to_array(image_load(valid_files[1])))
+
+img_to_score <- pblapply(valid_files, function(img) {
+  out <- image_load(img)
+  out <- image_to_array(out)
+  out <- array_reshape(out, c(1, img_dims))
+  out <- out / 255
+  out
+})
+
+img_to_score <- abind::abind(img_to_score, along = 1)
+
+predicted_probs <- data.frame(pred_prob = predict_proba(model, img_to_score))
+predicted_probs[["img_name"]] <- valid_files
+predicted_probs[["truth"]] <- as.numeric(!stringr::str_detect(valid_files, "notower"))
+
+write.csv(predicted_probs,
+          file = file.path(params$curr_model_dir, "predicted-probs.csv"),
+          row.names = FALSE)
+
+params$scoring_time <- Sys.time() - before
+message("Image scoring took ",
+        round(params$scoring_time, 3),
+        " ",
+        attr(params$scoring_time, "units"),
+        " to train.")
+
+# score final layer activations if small layer present
+if (params$add_small_final_layer) {
+  layer_output <- model$layers[[7]]$output
+
+  activation_model <- keras_model(inputs = model$input, outputs = layer_output)
+
+  my_activations <- activation_model %>%
+    predict(img_to_score)
+
+  img_activations <- as.data.frame(my_activations)
+  names(img_activations) <- paste0("neuron", seq_len(length(img_activations)))
+  img_activations[["img_name"]] <- valid_files
+  img_activations[["truth"]] <- as.numeric(!stringr::str_detect(valid_files, "notower"))
+
+  write.csv(img_activations,
+            file = file.path(params$curr_model_dir, "final-layer-activations.csv"),
+            row.names = FALSE)
+}
+
+#### examine scored test images ----------------------------
+predicted_probs <- read.csv(file.path(params$curr_model_dir, "predicted-probs.csv"),
+                            stringsAsFactors = FALSE)
+
+pred <- prediction(predicted_probs$pred_prob, predicted_probs$truth)
+
+message("\nFinal model performance measures:\n",
+        "   ROC AUC = ",
+        round(performance(pred, "auc")@y.values[[1]], 3), "\n",
+        "   Max possible accuracy = ",
+        round(max(performance(pred, "acc")@y.values[[1]]), 3)
+)
+
+ggplot(predicted_probs) +
+  geom_histogram(aes(x = pred_prob, y = ..count../sum(..count..),
+                     fill = factor(truth)),
+                 binwidth = .01, color = NA, alpha = .4) +
+  scale_fill_brewer(palette = "Dark2",
+                    guide = guide_legend(title = "Truth")) +
+  ggtitle("Model predicted probabilities for test set, by actual tower presence") +
+  xlab("Predicted probability") +
+  ylab("Proportion of all images") +
+  theme_minimal()
+}

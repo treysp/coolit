d68e7c3d0027b5e6d76e41f578bf9ad6edfb9054
Trey Spiller <treyspiller@gmail.com>
2019-03-31 16:32:31 -0400

    Address duplicated image and aux file names

--- R/score_jp2_image_dir.R
+++ R/score_jp2_image_dir.R
@@ -49,11 +49,18 @@ score_jp2_image_dir <- function(jp2_dir, jp2_aux_dir,
   stub <- str_match(jp2_files, "(.*/)(.*?)\\.jp2$")
   stub <- stub[, ncol(stub)]
 
+  if (any(duplicated(stub))) {
+    warning("Duplicated jp2 file stubs found. ",
+            "Only scoring first file for each duplicated stub.")
+  }
+
   jp2_file_names <- data.frame(
     stub = stub,
     img_file = jp2_files,
     stringsAsFactors = FALSE
   )
+
+  jp2_file_names <- jp2_file_names[!duplicated(jp2_file_names$stub),]
 
   jp2_aux_files <- list.files(jp2_aux_dir, full.names = TRUE, recursive = TRUE)
   jp2_aux_files <- jp2_aux_files[grepl("\\.jp2\\.aux\\.xml$", jp2_aux_files)]
@@ -61,11 +68,18 @@ score_jp2_image_dir <- function(jp2_dir, jp2_aux_dir,
   stub <- str_match(jp2_aux_files, "(.*/)(.*?)\\.jp2\\.aux\\.xml$")
   stub <- stub[, ncol(stub)]
 
+  if (any(duplicated(stub))) {
+    warning("Duplicated jp2 file stubs found. ",
+            "Only scoring first file for each duplicated stub.")
+  }
+
   jp2_aux_files <- data.frame(
     stub = stub,
     aux_file = jp2_aux_files,
     stringsAsFactors = FALSE
   )
+
+  jp2_aux_files <- jp2_aux_files[!duplicated(jp2_aux_files$stub),]
 
   jp2_file_names <- merge(jp2_file_names, jp2_aux_files, by = "stub")
 

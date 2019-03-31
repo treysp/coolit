1a9636aed743213bf513fc416190a254f10c05e5
Trey Spiller <treyspiller@gmail.com>
2019-03-31 16:13:01 -0400

    Fix image name stub regex

--- R/score_jp2_image_dir.R
+++ R/score_jp2_image_dir.R
@@ -45,16 +45,24 @@ score_jp2_image_dir <- function(jp2_dir, jp2_aux_dir,
   # make paired list of img and aux files
   jp2_files <- list.files(jp2_dir, full.names = TRUE, recursive = TRUE)
   jp2_files <- jp2_files[grepl("\\.jp2$", jp2_files)]
+
+  stub <- str_match(jp2_files, "(.*/)(.*?)\\.jp2$")
+  stub <- stub[, ncol(stub)]
+
   jp2_file_names <- data.frame(
-    stub = str_match(jp2_files, "(.*?)\\.jp2$")[, 2],
+    stub = stub,
     img_file = jp2_files,
     stringsAsFactors = FALSE
   )
 
   jp2_aux_files <- list.files(jp2_aux_dir, full.names = TRUE, recursive = TRUE)
   jp2_aux_files <- jp2_aux_files[grepl("\\.jp2\\.aux\\.xml$", jp2_aux_files)]
+
+  stub <- str_match(jp2_aux_files, "(.*/)(.*?)\\.jp2\\.aux\\.xml$")
+  stub <- stub[, ncol(stub)]
+
   jp2_aux_files <- data.frame(
-    stub = str_match(jp2_aux_files, "(.*?)\\.jp2\\.aux\\.xml$$")[, 2],
+    stub = stub,
     aux_file = jp2_aux_files,
     stringsAsFactors = FALSE
   )

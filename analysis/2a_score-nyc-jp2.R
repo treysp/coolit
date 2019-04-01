bf6faf870467099f684a91d18ed9360b376ab229
Trey Spiller <treyspiller@gmail.com>
2019-04-01 09:44:49 -0400

    Use F drive that has tons of storage space

--- analysis/2a_score-nyc-jp2.R
+++ analysis/2a_score-nyc-jp2.R
@@ -7,7 +7,7 @@ scores <- score_jp2_image_dir(
   jp2_aux_dir = "data/source_from-nyc-website/nyc_ortho_jp2/",
   model_params_dput_file = "output/multi-model-runs/2019-03-04/models/2019-03-04_20-49-33/run-parameters_dput.txt",
   model_h5_weights = "output/multi-model-runs/2019-03-04/models/2019-03-04_20-49-33/model_fine-tune-2.h5",
-  score_outdir = "output/2019-03-31/scored_nyc",
+  score_outdir = "F:/coolit/output/2019-03-31/scored_nyc",
   return_score = FALSE
 )
 

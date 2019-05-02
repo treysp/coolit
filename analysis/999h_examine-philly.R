57882f1526872abd5d570b05bb20d045e8b294f3
Trey Spiller <treyspiller@gmail.com>
2019-05-02 11:55:11 -0400

    Make a bunch of plots and write out poorly scored slices

--- analysis/999h_examine-philly.R
+++ analysis/999h_examine-philly.R
@@ -1,7 +1,7 @@
 library(sf)
 library(raster)
 library(mapview)
-library(data.table)
+library(magick)
 
 # get image crs
 philly_crs <- brick(
@@ -11,6 +11,12 @@ philly_crs <- crs(philly_crs)@projargs
 
 # get scores
 scores <- readRDS("output/2019-04-18/scored_philly/philly-scores-gt0338-lte1.rds")
+
+tower_coord_buff <- st_buffer(tower_coord$geometry, 25)
+
+slice_tower_intersect <- st_intersects(scores, tower_coord_buff)
+
+
 
 # create tower coords sf
 tower_coord <- readxl::read_excel(
@@ -34,7 +40,6 @@ philly_intersect <- st_intersects(philly_index, tower_coord)
 philly_index$hastower_coord <- sapply(philly_intersect, function(x) length(x) != 0)
 
 # make plots
-# make plot
 make_tower_plot <- function(tower_img_name, scores_sf, tower_coord_sf = NULL, outpath) {
   tower_img <- brick(tower_img_name)
 
@@ -102,5 +107,175 @@ pbapply::pblapply(X = plot_img_names, cl = cl, FUN = function(x) {
                   tower_coord,
                   "output/2019-04-18/scored_philly/tower_plots")
 })
+
+parallel::stopCluster(cl)
+
+# save slices to identify false positives
+plot_scores <- scores[scores$predicted_probs >= 0.3 &
+                        scores$predicted_probs < 0.4,]
+
+bad_towers <- data.frame(
+  img_id = stringr::str_match(plot_scores$source_img, "(.*/)*(.*)\\.(tif|png|jp2)")[,3],
+  slice_id = plot_scores$slice_id,
+  stringsAsFactors = FALSE
+)
+
+bad_towers <- bad_towers[order(bad_towers$img_id), ]
+
+cl <- parallel::makeCluster(parallel::detectCores() - 1)
+
+parallel::clusterEvalQ(cl, {
+  library(magick)
+})
+
+pbapply::pblapply(X = split(bad_towers, bad_towers$img_id), cl = cl,
+                  FUN = function(x) {
+  img_id <- unique(x$img_id)
+
+  img_df <- readRDS(file.path("output/2019-04-18/sliced_philly",
+                              paste0(img_id, "_slices.rds")))
+
+  img_df <- img_df[img_df$slice_id %in% x$slice_id,]
+
+  for (i in seq_len(nrow(img_df))) {
+    magick::image_write(
+      image = image_read(drop(img_df[i, "slice_array"][[1]]) / 255),
+      path = file.path("output/2019-04-18/slices_gte03_lt04",
+                       paste0(img_id, "_", img_df[i, "slice_id"], ".png")))
+  }
+
+})
+
+parallel::stopCluster(cl)
+
+# save slices that intersect with tower coords + 25-ft radius
+tower_coord_buff <- st_buffer(tower_coord$geometry, 25)
+
+slices <- c(
+  "philly-scores-lte001.rds",
+  "philly-scores-gt001-lte0013.rds", "philly-scores-gt0013-lte0018.rds",
+  "philly-scores-gt0018-lte0025.rds", "philly-scores-gt0025-lte0034.rds",
+  "philly-scores-gt0034-lte0048.rds", "philly-scores-gt0048-lte007.rds",
+  "philly-scores-gt007-lte0106.rds", "philly-scores-gt0106-lte0174.rds",
+  "philly-scores-gt0174-lte0338.rds", "philly-scores-gt0338-lte1.rds"
+  )
+
+for (slice in slices) {
+
+  scores <- readRDS(file.path("output/2019-04-18/scored_philly/", slice))
+
+  slice_tower_intersect <- st_intersects(scores, tower_coord_buff)
+
+  slices_intersect <- scores[
+    sapply(slice_tower_intersect, function(x) length(x) > 0) == TRUE,
+  ]
+
+  slices_intersect <- data.frame(
+    img_id = stringr::str_match(
+      slices_intersect$source_img, "(.*/)*(.*)\\.(tif|png|jp2)")[,3],
+    slice_id = slices_intersect$slice_id,
+    stringsAsFactors = FALSE
+  )
+
+  cl <- parallel::makeCluster(length(unique(slices_intersect$img_id)))
+
+  parallel::clusterEvalQ(cl, {
+    library(magick)
+  })
+
+  pbapply::pblapply(X = split(slices_intersect, slices_intersect$img_id), cl = cl,
+                    FUN = function(x) {
+                      img_id <- unique(x$img_id)
+
+                      img_df <- readRDS(file.path("output/2019-04-18/sliced_philly",
+                                                  paste0(img_id, "_slices.rds")))
+
+                      img_df <- img_df[img_df$slice_id %in% x$slice_id,]
+
+                      for (i in seq_len(nrow(img_df))) {
+                        magick::image_write(
+                          image = image_read(drop(img_df[i, "slice_array"][[1]]) / 255),
+                          path = file.path("output/2019-04-18/slices_tower-coord-intersect",
+                                           paste0(img_id, "_", img_df[i, "slice_id"], ".png")))
+                      }
+
+                    })
+
+  parallel::stopCluster(cl)
+}
+
+# examine towers with coords and those without
+slices_nocoord <- list.dirs("output/2019-04-18", recursive = TRUE, full.names = TRUE)
+slices_nocoord <- slices_nocoord[grepl("gte", slices_nocoord)]
+slices_nocoord <- slices_nocoord[grepl("/tower", slices_nocoord)]
+slices_nocoord <- list.files(slices_nocoord)
+
+slices_coord <- list.files("output/2019-04-18/slices_tower-coord-intersect/tower")
+
+slices_i_missed <- setdiff(slices_coord, slices_nocoord)
+slices_they_missed <- setdiff(slices_nocoord, slices_coord)
+
+# save all tower slices to curated training data
+all_tower_slices <- unique(c(slices_coord, slices_nocoord))
+
+all_tower_slices <- data.frame(
+  img_id = stringr::str_match(all_tower_slices, "(.*6in)_(\\d*?)\\.png")[, 2],
+  slice_id = stringr::str_match(all_tower_slices, "(.*6in)_(\\d*?)\\.png")[, 3],
+  stringsAsFactors = FALSE
+)
+
+cl <- parallel::makeCluster(parallel::detectCores() - 1)
+
+pbapply::pblapply(
+  X = split(all_tower_slices, all_tower_slices$img_id), cl = cl,
+            FUN = function(x) {
+              img_id <- unique(x$img_id)
+
+              img_df <- readRDS(file.path("output/2019-04-18/sliced_philly",
+                                          paste0(img_id, "_slices.rds")))
+
+              img_df <- img_df[img_df$slice_id %in% x$slice_id,]
+
+              saveRDS(
+                img_df,
+                file.path("data/curated-training-slices/philly-towers",
+                          paste0(img_id, "_tower_slices.rds"))
+                )
+
+            })
+
+parallel::stopCluster(cl)
+
+# save all high prob notower slices to curated training data
+slices_notower <- list.dirs("output/2019-04-18", recursive = TRUE, full.names = TRUE)
+slices_notower <- slices_notower[grepl("gte", slices_notower)]
+slices_notower <- slices_notower[grepl("/notower", slices_notower)]
+slices_notower <- list.files(slices_notower)
+
+all_notower_slices <- data.frame(
+  img_id = stringr::str_match(slices_notower, "(.*6in)_(\\d*?)\\.png")[, 2],
+  slice_id = stringr::str_match(slices_notower, "(.*6in)_(\\d*?)\\.png")[, 3],
+  stringsAsFactors = FALSE
+)
+
+cl <- parallel::makeCluster(parallel::detectCores() - 1)
+
+pbapply::pblapply(
+  X = split(all_notower_slices, all_notower_slices$img_id), cl = cl,
+  FUN = function(x) {
+    img_id <- unique(x$img_id)
+
+    img_df <- readRDS(file.path("output/2019-04-18/sliced_philly",
+                                paste0(img_id, "_slices.rds")))
+
+    img_df <- img_df[img_df$slice_id %in% x$slice_id,]
+
+    saveRDS(
+      img_df,
+      file.path("data/curated-training-slices/philly/philly-highprob-notowers",
+                paste0(img_id, "_highprob-notower_slices.rds"))
+    )
+
+  })
 
 parallel::stopCluster(cl)

ffe28b0ba9376bddbea0ebf3462fb3ae4e327205
Trey Spiller <treyspiller@gmail.com>
2019-05-16 11:31:07 -0400

    Make rds versions of tower slices and high-prob nontower slices

--- analysis/4d_inspect-chi.R
+++ analysis/4d_inspect-chi.R
@@ -60,7 +60,7 @@ model_towers <- data.frame(
   model_tower = 1
 )
 
-hand_towers <- fs::dir_ls("data/source_from-chi-website/tower-slice-images", type = "file")
+hand_towers <- fs::dir_ls("data/curated-training-slices/chi/chi-tower-images", type = "file")
 
 hand_towers <- str_match(hand_towers, "(.*/)*(\\d{1,4})_(\\d{1,4})\\.png$")[, 3:4]
 
@@ -70,12 +70,9 @@ hand_towers <- data.frame(
   hand_tower = 1
 )
 
-all_towers <- dplyr::full_join(model_towers, hand_towers,
-                               by = c("img_id", "slice_id"))
+hand_towers <- anti_join(hand_towers, model_towers)
 
-both_towers <- filter(all_towers, model_tower == 1 & hand_tower == 1)
-model_only_towers <- filter(all_towers, model_tower == 1 & is.na(hand_tower))
-hand_only_towers <- filter(all_towers, is.na(model_tower) & hand_tower == 1)
+all_towers <- bind_rows(model_towers, hand_towers)
 
 my_scores <- readRDS("output/2019-05-07/chi-scores/chi-scores-gt0417-lte1.rds")
 my_scores$img_id <- as.numeric(
@@ -83,9 +80,9 @@ my_scores$img_id <- as.numeric(
             "(.*/)*(\\d{1,4})_slices_scores\\.rds$")[, 3]
   )
 
-hand_only_towers <- left_join(hand_only_towers,
-                              my_scores[, c("img_id", "slice_id", "predicted_probs")],
-                              by = c("img_id", "slice_id"))
+hand_towers <- left_join(hand_towers,
+                         my_scores[, c("img_id", "slice_id", "predicted_probs")],
+                         by = c("img_id", "slice_id"))
 
 hand_slices <- fs::dir_ls("c:/users/wfu3/desktop/temp/notower",
                           type = "file", recursive = TRUE)
@@ -98,3 +95,80 @@ hand_slices <- data.frame(
 
 model_only_towers <- left_join(model_only_towers, hand_slices,
                                by = c("img_id", "slice_id"))
+
+# make highprob-notower slices
+my_scores <- readRDS("output/2019-05-07/chi-scores/chi-scores-gt0417-lte1.rds")
+my_scores$img_id <- as.numeric(
+  str_match(my_scores$img_name,
+            "(.*/)*(\\d{1,4})_slices_scores\\.rds$")[, 3]
+)
+
+all_towers <- fs::dir_ls("data/curated-training-slices/chi/chi-tower-images", type = "file")
+
+all_towers <- str_match(all_towers, "(.*/)*(\\d{1,4})_(\\d{1,4})\\.png$")[, 3:4]
+
+all_towers <- data.frame(
+  img_id = as.numeric(all_towers[, 1]),
+  slice_id = as.numeric(all_towers[, 2])
+)
+
+my_scores_90 <- my_scores[my_scores$predicted_probs > .9,]
+
+my_scores_90 <- anti_join(my_scores_90, all_towers, by = c("img_id", "slice_id"))
+my_scores_90$geometry <- NULL
+my_scores_90 <- my_scores_90[, c("img_id", "slice_id")]
+my_scores_90$highprob_notower <- TRUE
+
+all_image_id <- unique(c(all_towers$img_id, my_scores_90$img_id))
+temp <- list.files("data/curated-training-slices/chi/chi-highprob-notower")
+temp <- str_match(temp, "(\\d{1,4}).*")[, 2]
+temp <- as.numeric(temp)
+all_image_id <- all_image_id[!(all_image_id %in% temp)]
+
+cl <- parallel::makeCluster(15)
+
+parallel::clusterEvalQ(cl, {
+  library(dplyr)
+  library(raster)
+  library(sf)
+})
+parallel::clusterExport(cl, c("all_towers", "my_scores_90"))
+
+pbapply::pblapply(X = all_image_id, cl = cl, FUN = function(img) {
+  temp_slices <- readRDS(paste0(
+    "data/curated-training-slices/chi/chi-slices/",
+    img,
+    "_slices.rds")
+  )
+
+  temp_slices$img_id <- img
+
+  temp_tower <- inner_join(temp_slices, all_towers, by = c("img_id", "slice_id"))
+
+  if (nrow(temp_tower) > 0) {
+    saveRDS(
+      temp_tower,
+      paste0(
+        "data/curated-training-slices/chi/chi-towers/",
+        img,
+        "_tower_slices.rds"
+      )
+    )
+  }
+
+  temp_notower <- inner_join(temp_slices, my_scores_90,
+                             by = c("img_id", "slice_id"))
+
+  if (nrow(temp_notower) > 0) {
+    saveRDS(
+      temp_notower,
+      paste0(
+        "data/curated-training-slices/chi/chi-highprob-notower/",
+        img,
+        "_highprob-notower_slices.rds"
+      )
+      )
+  }
+})
+
+parallel::stopCluster(cl)

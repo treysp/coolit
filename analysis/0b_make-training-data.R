d0bd71a5d4b732a7fce2dfa4a1d9fb31c2ef3ece
Trey Spiller <treyspiller@gmail.com>
2019-05-17 14:12:32 -0400

    Add chicago training data

--- analysis/0b_make-training-data.R
+++ analysis/0b_make-training-data.R
@@ -10,7 +10,9 @@ set.seed(1)
 num_notower <- 10000
 
 # where images should live
-base_dir <- "data/model-training-data/slices_curated_2019-04-22"
+base_dir <- "data/model-training-data/slices_curated_2019-05-16"
+
+dir.create(base_dir)
 
 for (i in c("train", "validation", "test")) {
 
@@ -23,14 +25,14 @@ for (i in c("train", "validation", "test")) {
 #### NYC preparation --------------------------------------------------------------------
 # get obstructed tower slice list
 nyc_obstructed <- readr::read_csv(
-  file.path("data/curated-training-slices/nyc/nyc-towers",
+  file.path("data/curated-training-slices/nyc/nyc-tower-slices",
             "nyc_obstructed-tower-slice-list_2019-04-23.csv"))
 
 nyc_obstructed$exclude <- TRUE
 
 # get tower slices
 nyc_tower_slices <- lapply(
-  list.files("data/curated-training-slices/nyc/nyc-towers",
+  list.files("data/curated-training-slices/nyc/nyc-tower-slices",
              full.names = TRUE, pattern = "\\.rds"),
   readRDS
 )
@@ -160,6 +162,66 @@ philly_highprob$out_name <- paste0(base_dir, "/",
                                    philly_highprob$img_num, "_",
                                    philly_highprob$slice_id, ".png")
 
+#### Chi preparation --------------------------------------------------------------------
+# get tower slices
+chi_tower_slices <- lapply(
+  list.files("data/curated-training-slices/chi/chi-towers",
+             full.names = TRUE, pattern = "\\.rds"),
+  readRDS
+)
+
+chi_tower_slices <- do.call("rbind", chi_tower_slices)
+
+chi_tower_slices$img_num <- chi_tower_slices$img_id
+
+# assign train/validation/test status
+chi_tower_slices$status <- sample(c("train", "validation", "test"),
+                                     size = nrow(chi_tower_slices),
+                                     prob = c(.7, .2, .1),
+                                     replace = TRUE)
+
+chi_tower_slices$out_name <- paste0(base_dir, "/",
+                                       chi_tower_slices$status, "/",
+                                       "tower/",
+                                       chi_tower_slices$img_num, "_",
+                                       chi_tower_slices$slice_id, ".png")
+
+chi_tower_index <- chi_tower_slices[, c("img_num", "slice_id")]
+chi_tower_index$exclude <- TRUE
+
+# count tower/img for notower sampling
+chi_tower_img_count <- table(chi_tower_slices$img_num)
+chi_tower_img_count <- data.frame(
+  img_num = names(chi_tower_img_count),
+  count = as.integer(chi_tower_img_count),
+  stringsAsFactors = FALSE
+)
+
+chi_tower_img_count$prop <- chi_tower_img_count$count /
+  sum(chi_tower_img_count$count)
+
+# get high-prob notower slices
+chi_highprob <- lapply(
+  list.files("data/curated-training-slices/chi/chi-highprob-notower",
+             full.names = TRUE, pattern = "\\.rds"),
+  readRDS
+)
+
+chi_highprob <- do.call("rbind", chi_highprob)
+
+chi_highprob$img_num <- chi_highprob$img_id
+
+chi_highprob$status <- sample(c("train", "validation", "test"),
+                                 size = nrow(chi_highprob),
+                                 prob = c(.7, .2, .1),
+                                 replace = TRUE)
+
+chi_highprob$out_name <- paste0(base_dir, "/",
+                                   chi_highprob$status, "/",
+                                   "notower/",
+                                   chi_highprob$img_num, "_",
+                                   chi_highprob$slice_id, ".png")
+
 #### write images ---------------------------------------------------------------------
 # setup cluster
 ncores <- parallel::detectCores() - 1
@@ -174,7 +236,8 @@ parallel::clusterEvalQ(cl, {
 
 parallel::clusterExport(cl, c("base_dir",
                               "nyc_tower_index", "nyc_tower_img_count",
-                              "philly_tower_index", "philly_tower_img_count"))
+                              "philly_tower_index", "philly_tower_img_count",
+                              "chi_tower_index", "chi_tower_img_count"))
 
 out <- list()
 
@@ -202,6 +265,18 @@ out[[2]] <- pbapply::pblapply(X = philly_tower_list, cl = cl, FUN = function(x) 
 
     magick::image_write(
       image = image_read(temp / 255),
+      path = x[i, "out_name", drop = TRUE])
+  }
+})
+
+## chi
+chi_tower_list <- split(chi_tower_slices,
+                        make_split_index(nrow(chi_tower_slices), ncores))
+
+out[[1]] <- pbapply::pblapply(X = chi_tower_list, cl = cl, FUN = function(x) {
+  for (i in seq_len(nrow(x))) {
+    magick::image_write(
+      image = image_read(drop(x[i, "slice_array", drop = TRUE][[1]]) / 255),
       path = x[i, "out_name", drop = TRUE])
   }
 })
@@ -234,9 +309,22 @@ out[[4]] <- pbapply::pblapply(X = philly_highprob_list, cl = cl, FUN = function(
   }
 })
 
+## chi
+chi_highprob_list <- split(chi_highprob,
+                           make_split_index(nrow(chi_highprob), ncores))
+
+out[[4]] <- pbapply::pblapply(X = chi_highprob_list, cl = cl, FUN = function(x) {
+  for (i in seq_len(nrow(x))) {
+    magick::image_write(
+      image = image_read(drop(x[i, "slice_array", drop = TRUE][[1]]) / 255),
+      path = x[i, "out_name", drop = TRUE])
+  }
+})
+
 # sample and write notower images
 tower_props <- c(nyc = nrow(nyc_tower_slices),
-                 philly = nrow(philly_tower_slices))
+                 philly = nrow(philly_tower_slices),
+                 chi = nrow(chi_tower_slices))
 
 tower_props <- tower_props / sum(tower_props)
 
@@ -250,7 +338,7 @@ nyc_tower_img_count_list <- split(
   nyc_tower_img_count$img_num
   )
 
-out[[5]] <- pbapply::pblapply(
+out[[5]] <- parallel::parLapplyLB(
   X = nyc_tower_img_count_list,
   cl = cl,
   FUN = function(rds) {
@@ -301,7 +389,7 @@ philly_tower_img_count_list <- split(
   philly_tower_img_count$img_num
 )
 
-out[[6]] <- pbapply::pblapply(
+out[[6]] <- parallel::parLapplyLB(
   X = philly_tower_img_count_list,
   cl = cl,
   FUN = function(rds) {
@@ -344,5 +432,58 @@ out[[6]] <- pbapply::pblapply(
       path = working[i, "out_name", drop = TRUE])
   }
 })
+
+## chi
+chi_tower_img_count$num_to_write <- ceiling(
+  chi_tower_img_count$prop * num_notower * tower_props[["chi"]]
+)
+
+chi_tower_img_count_list <- split(
+  chi_tower_img_count,
+  chi_tower_img_count$img_num
+)
+
+chi_tower_index$geometry <- NULL
+
+
+out[[5]] <- parallel::parLapplyLB(
+  X = chi_tower_img_count_list,
+  cl = cl,
+  fun = function(rds) {
+
+    working_name <- file.path("data/curated-training-slices/chi/chi-slices",
+                              paste0(unique(rds$img_num), "_slices.rds"))
+
+    working <- readRDS(working_name)
+
+    working$img_num <- unique(rds$img_num)
+
+    working <- merge(working,
+                     chi_tower_index,
+                     by = c("img_num", "slice_id"),
+                     all.x = TRUE)
+    working <- working[is.na(working$exclude), ]
+
+    num_to_write <- rds$num_to_write
+
+    working <- working[sample(seq_len(nrow(working)), num_to_write), ]
+
+    working$status <- sample(c("train", "validation", "test"),
+                             size = nrow(working),
+                             prob = c(.7, .2, .1),
+                             replace = TRUE)
+
+    working$out_name <- paste0(base_dir, "/",
+                               working$status, "/",
+                               "notower/",
+                               working$img_num, "_",
+                               working$tile_id, ".png")
+
+    for (i in seq_len(nrow(working))) {
+      magick::image_write(
+        image = image_read(drop(working[i, "slice_array", drop = TRUE][[1]]) / 255),
+        path = working[i, "out_name", drop = TRUE])
+    }
+  })
 
 parallel::stopCluster(cl)
